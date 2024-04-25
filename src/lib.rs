ad2c9d775728fab33fba050ca7f7b84911bc6bbd
higersky <64680426+higersky@users.noreply.github.com>
2024-04-25 13:05:31 +0800

    Use windows-sys to merge pull request #9 from scullionw/dirstat-rs

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,30 @@ impl DiskItem {
         apparent: bool,
         root_dev: u64,
     ) -> Result<Self, Box<dyn Error>> {
+        #[cfg(windows)]
+        {
+            // Solution for windows compressed files requires path to be absolute, see ffi.rs
+            // Basically it would be triggered only on top most invocation,
+            // and afterwards all path would be absolute. We do it here as it is relatively harmless
+            // but this would allow us fo it only once instead of each invocation of ffi::compressed_size
+            if apparent && !path.is_absolute() {
+                use path_absolutize::*;
+                let absolute_dir = path.absolutize()?;
+                return Self::analyze(
+                    absolute_dir.as_ref(),
+                    apparent,
+                    root_dev,
+                );
+            }
+        }
+        return Self::analyze(path, apparent, root_dev);
+    }
+
+    fn analyze(
+        path: &Path,
+        apparent: bool,
+        root_dev: u64,
+    ) -> Result<Self, Box<dyn Error>> {
         let name = path
             .file_name()
             .unwrap_or(&OsStr::new("."))
@@ -115,3 +139,6 @@ impl FileInfo {
         }
     }
 }
+
+#[cfg(test)]
+mod tests;
